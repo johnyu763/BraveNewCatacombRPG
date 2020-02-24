f3228f6e6f102d9d18ab5c40c05c92cdbc6e3fc4
AaronShah2 <47093449+AaronShah2@users.noreply.github.com>
2020-02-24 13:07:18 -0800

    I forgot

---
+++ Assets/Scripts/KillEnemy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class KillEnemy : MonoBehaviour {
+ 
+    public GameObject menuItem;
+ 
+    void OnDestroy() {
+        Destroy (this.menuItem);
+    }
+}
