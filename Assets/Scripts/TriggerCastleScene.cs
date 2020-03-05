927508419bf3b4134da97b36a48d1f5cdfd1f800
Sienna Brown <31221385+siennana@users.noreply.github.com>
2020-03-04 18:49:31 -0800

    interior castle update

--- Assets/Scripts/TriggerCastleScene.cs
+++ Assets/Scripts/TriggerCastleScene.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class TriggerCastleScene : MonoBehaviour
 {
+    public GameObject Unity;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Testing castle door collider");
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if(Vector3.Distance(Unity.transform.position, this.transform.position) < 50f)
+        {
+            Debug.Log("Collision!");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
